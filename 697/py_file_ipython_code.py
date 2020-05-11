25797fdca40074b58b5bbae9a06d0a73ab541c88
Gavara Tarun <tgavara@qubole.com>
2020-05-11 12:02:27 +0530

    change

--- 697/py_file_ipython_code.py
+++ 697/py_file_ipython_code.py
@@ -1,5 +1,5 @@
 # This is a python file containing valid code with no errors.
-%pycat https://github.com/tarungavara/sample-repo/697/py_file_ipython_code.py
+%pycat https://github.com/tarungavara/sample-repo/raw/master/697/py_file_ipython_code.py
 
 def add(a, b):
     return a+b
