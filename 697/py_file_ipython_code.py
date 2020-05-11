1ad648c17920f63af327249bbe19d32b57f7d585
Gavara Tarun <tgavara@qubole.com>
2020-05-11 11:56:50 +0530

    add files for 697

---
+++ 697/py_file_ipython_code.py
@@ -0,0 +1,8 @@
+# This is a python file containing valid code with no errors.
+%pycat https://github.com/tarungavara/sample-repo/697/py_file_ipython_code.py
+
+def add(a, b):
+    return a+b
+
+if __name__ == '__main__':
+    print('try add(3,4)')
---
+++ 697/py_file_no_error.py
@@ -0,0 +1,11 @@
+# This is a python file containing valid code with no errors.
+print('i hope you have a variable called x initialized.')
+print('value of x is {}'.format(x))
+x = 10
+print('value of x is now {}'.format(x))
+
+def add(a, b):
+    return a+b
+
+if __name__ == '__main__':
+    print('try add(3,4)')
